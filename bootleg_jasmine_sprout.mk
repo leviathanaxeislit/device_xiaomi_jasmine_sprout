86d6c97a584fbba43e5506dff7bfc2117d465838
Leviathanaxeislit <scp.thedevil@gmail.com>
2019-10-25 10:05:10 +0530

    Jasmine_sprout: Inline Gapps
    Signed-off-by: Leviathanaxeislit <scp.thedevil@gmail.com>

--- bootleg_jasmine_sprout.mk
+++ bootleg_jasmine_sprout.mk
@@ -26,7 +26,10 @@ BUILD_TWRP := true
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit some common bootleggers stuff.
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/gapps/config.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
